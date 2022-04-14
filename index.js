781ddb7df51c01cb2490ef0f5b448f16401b2b2d
Precious <preciousmaregwa1997@gmail.com>
2022-04-14 21:01:37 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code here!document.getElementById("main").remove();
+document.getElementById("main").remove();
+const newHeader  = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Precious is the champion"
